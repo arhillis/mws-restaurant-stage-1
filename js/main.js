5d7d05f2a44473416154b59730ccf5b80754f7e8
Andrielle Hillis <andriellerhillis@gmail.com>
2018-10-15 08:01:32 -0500

    Making some suggested changes to main.js, including adding accessability information to the details buttons

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = `Image of ${image.name}`;
   li.append(image);
 
   const name = document.createElement('h1');
@@ -175,11 +176,15 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
-  
+  const btn = document.createElement('button');  
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more);
+  more.tabIndex = '0';
+  more.ariaLabel = `View details of ${restaurant.name}`;
+
+  btn.append(more);
+  li.append(btn);
 
   return li;
 }
